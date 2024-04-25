f92e07c5eecc697036fd5421032bc631800098e4
Amber Lea <112793091+Amber-Lea-P@users.noreply.github.com>
2024-04-25 04:55:07 +0300

    adding the profile photo to the profile page. Minor changes to the UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className='flex flex-col'>
+        <img src={currentUser.profilePicture} alt="Profile Photo" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover' />
+      </form>
+    </div>
+  );
 }
